7cb0232c83ca0536fcf42f57b10ff85805b26f6b
mddazure <mddazure@gmail.com>
2020-12-15 16:02:18 +0100

    updates

--- private-link-endpoint.tf
+++ private-link-endpoint.tf
@@ -46,6 +46,7 @@ resource "azurerm_subnet" "ple-subnet" {
  resource_group_name = azurerm_resource_group.privatelink-endpoint-rg.name
   virtual_network_name = azurerm_virtual_network.privatelink-endpoint-vnet.name
   address_prefixes       = ["192.168.0.128/27"]
+  enforce_private_link_service_network_policies = true
 }
 #######################################################################
 ## Create Network Interface - client-1-nic
