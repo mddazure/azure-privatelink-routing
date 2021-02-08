fd2b33a109c57d93de3c4b27913e0f663a3b51c2
mddazure <mddazure@gmail.com>
2021-02-08 12:02:06 +0100

    updates

--- lab/private-link-service.tf
+++ lab/private-link-service.tf
@@ -270,7 +270,6 @@ resource "azurerm_lb" "lb-1" {
   }
 }
 resource "azurerm_lb_backend_address_pool" "lb-1-bepool" {
-  resource_group_name   = azurerm_resource_group.privatelink-service-rg.name
   loadbalancer_id     = azurerm_lb.lb-1.id
   name                = "lb-1-bepool"
 }
